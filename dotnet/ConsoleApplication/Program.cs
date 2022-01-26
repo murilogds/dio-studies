7c727d18ee70b11231fe28b253a5971e9d6f2e85
Murilo Gomes <murilo.goomes@hotmail.com>
2022-01-25 23:03:39 -0300

    create console base application

---
+++ dotnet/ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int times = 5;
+
+      for (int i = 0; i < times; i++)
+      {
+        Console.WriteLine($"Loop nº {i}");
+      }
+    }
+  }
+}
